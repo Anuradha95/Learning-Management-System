966ea773e1797832bdc29e08405c69b1a0878dcd
Anuradha Subasinghe <subasinghe963@gmail.com>
2022-06-10 22:02:04 +0530

    updated server file

--- Backend/models/student.model.js
+++ Backend/models/student.model.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-  studentName: { type: String, required: true, unique: true, trim: true, minlength: 3},
-  studentGrade: { type: String, required: true, unique: true, trim: true, maxlength: 2},
+  studentName: { type: String, required: true, unique: true, minlength: 3},
+  studentGrade: { type: String, required: true, unique: true, maxlength: 2},
+  date: { type: Date, required: true },
 }, {
   timestamps: true,
 });
--- Backend/server.js
+++ Backend/server.js
@@ -10,14 +10,19 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect("mongodb://localhost:27017/lmsDb", { useNewUrlParser: true });
+
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+});
+
+const studentsRouter = require('./routes/students');
+
+app.use('/students', studentsRouter);
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
 
-const uri = "mongodb://localhost:27017/lmsDb"
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true }
-);
-const connection = mongoose.connection;
-connection.once('open', () => {
-  console.log("MongoDB database connection established successfully");
-})
