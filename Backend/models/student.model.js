3c6961dcabd3be2a340c9e72c913688bb957f5e5
Anuradha Subasinghe <subasinghe963@gmail.com>
2022-06-11 18:41:07 +0530

    updated student model file

--- Backend/models/student.model.js
+++ Backend/models/student.model.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const studentSchema = new Schema({
-  studentName: { type: String, required: true, unique: true, minlength: 3},
-  studentGrade: { type: String, required: true, unique: true, maxlength: 2},
-  date: { type: Date, required: true },
+  studentName: { type: String, unique: true, minlength: 3},
+  studentGrade: { type: String, unique: true, maxlength: 2},
+  date: { type: String},
 }, {
   timestamps: true,
 });
